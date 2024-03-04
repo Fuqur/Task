391567804c143b5d05663c69effd2da9134205a4
Fuqur <142494543+Fuqur@users.noreply.github.com>
2024-03-04 17:20:43 +0300

    added index.tsx

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from './components/layout'
+
+export default function Home() {
+  return (
+    <Layout>
+      <h1>Welcome to my Book Database!</h1>
+      <p>This is a web application for book database</p>
+      <p>You can browse the books, or log in as an admin to create and edit them.</p>
+    </Layout>
+  )
+}
