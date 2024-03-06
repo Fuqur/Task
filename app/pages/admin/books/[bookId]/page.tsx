f4ac4d7940a2bf2afd4da81967a97b4d35664f65
Fuqur <142494543+Fuqur@users.noreply.github.com>
2024-03-06 19:13:16 +0300

    updated page

--- app/pages/admin/books/[bookId]/page.tsx
+++ app/pages/admin/books/[bookId]/page.tsx
@@ -1,41 +1,16 @@
 import React from 'react';
-import { GetServerSideProps } from 'next';
-import { fetchBookById } from '../../../../api'; 
+import { useRouter } from 'next/router';
 
-interface Book {
-  _id: string;
-  title: string;
-  author: string;
-  cover: string;
-  description: string;
-}
+const BookDetailPage: React.FC = () => {
+  const router = useRouter();
+  const { bookId } = router.query;
 
-const AdminBookDetailPage: React.FC<{ book: Book }> = ({ book }) => {
   return (
     <div>
-      <h1>Edit Book: {book.title}</h1>
+      <h1>Book Detail Page</h1>
+      <p>Book ID: {bookId}</p>
     </div>
   );
 };
 
-export default AdminBookDetailPage;
-
-export const getServerSideProps: GetServerSideProps = async (context) => {
-  const { params } = context;
-  const bookId = params?.bookId as string;
-
-  if (!bookId) {
-    return {
-      notFound: true, 
-    };
-  }
-
-  const res = await fetchBookById(bookId);
-  const book: Book = await res.json();
-
-  return {
-    props: {
-      book,
-    },
-  };
-};
+export default BookDetailPage;
--- app/pages/books/page.tsx
+++ app/pages/books/page.tsx
@@ -1,42 +1,11 @@
 import React from 'react';
-import Link from 'next/link';
-import { GetServerSideProps } from 'next';
-import { fetchAllBooks } from '../../api';
 
-interface Book {
-  _id: string;
-  title: string;
-  author: string;
-  cover: string;
-  description: string;
-}
-
-const AdminBooksPage: React.FC<{ books: Book[] }> = ({ books }) => {
+const Page: React.FC = () => {
   return (
     <div>
-      <h1>Admin Books Page</h1>
-      <ul>
-        {books.map((book) => (
-          <li key={book._id}>
-            <Link href={`/admin/books/${book._id}`}>
-              <a>{book.title}</a>
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <h1>All Books</h1>
     </div>
   );
 };
 
-export default AdminBooksPage;
-
-export const getServerSideProps: GetServerSideProps = async () => {
-  const res = await fetchAllBooks();
-  const books: Book[] = await res.json();
-
-  return {
-    props: {
-      books,
-    },
-  };
-};
+export default Page;
