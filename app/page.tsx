48bf148abe66e512551e8bc8f67f46d679af2e63
Fuqur <142494543+Fuqur@users.noreply.github.com>
2024-03-07 17:39:28 +0300

    added page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to My BookPage</h1>
+      <p>Enjoy your search</p>
+    </div>
+  );
+};
+
+export default HomePage;
