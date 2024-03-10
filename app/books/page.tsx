cd4243862e7eebbc5a32729607042178db0725df
Fuqur <142494543+Fuqur@users.noreply.github.com>
2024-03-10 20:32:54 +0300

    updated page.tsx

--- app/books/page.tsx
+++ app/books/page.tsx
@@ -1,47 +1,27 @@
 import React from 'react';
 import Link from 'next/link';
 
-interface Book {
-  id: string;
-  title: string;
-  author: string;
-  cover: string;
-  description: string;
-}
-
-const Page: React.FC<{ books: Book[] }> = ({ books }) => {
-  return (
-    <div>
-      <h1>Books</h1>
-      {books.map((book) => (
-        <div key={book.id}> 
-          <Link href={`/books/${book.id}`}> 
-            <a>{book.title}</a>
-          </Link>
-        </div>
-      ))}
-    </div>
-  );
-};
-
-export default Page;
-
-export async function getServerSideProps() {
+export default async function Page() {
   try {
-    const res = await fetch('https://crudcrud.com/api/c6ee99d6cfcb423da8aa3c918e72dd53');
-    const books: Book[] = await res.json();
-
-    return {
-      props: {
-        books,
-      },
-    };
-  } catch (error) {
-    console.error('Error fetching data:', error);
-    return {
-      props: {
-        books: [],
-      },
-    };
+    const response = await fetch('https://crudcrud.com/api/c6ee99d6cfcb423da8aa3c918e72dd53');
+    if (!response.ok) {
+      throw new Error('Failed to fetch data');
+    }
+    const data = await response.json();
+    
+    return (
+      <div>
+        <h1>Books</h1>
+        {data.map((book) => (
+          <div key={book.id}>
+            <Link href={`/books/${book.id}`}>
+              <a>{book.title}</a>
+            </Link>
+          </div>
+        ))}
+      </div>
+    );
+  } catch (error: any) {
+    return <div>Failed to load: {(error as Error).message}</div>; 
   }
 }
