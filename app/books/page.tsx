6fb1f3983aecad02f1c03ec90d40aa0245382f4f
Fuqur <142494543+Fuqur@users.noreply.github.com>
2024-03-10 21:24:59 +0300

    updated layout and page

--- app/books/page.tsx
+++ app/books/page.tsx
@@ -1,27 +1,20 @@
-import React from 'react';
-import Link from 'next/link';
+import React from "react";
+import Link from "next/link";
+import { getBooks } from "../utils";
 
 export default async function Page() {
-  try {
-    const response = await fetch('https://crudcrud.com/api/c6ee99d6cfcb423da8aa3c918e72dd53');
-    if (!response.ok) {
-      throw new Error('Failed to fetch data');
-    }
-    const data = await response.json();
-    
-    return (
-      <div>
-        <h1>Books</h1>
-        {data.map((book) => (
-          <div key={book.id}>
-            <Link href={`/books/${book.id}`}>
-              <a>{book.title}</a>
-            </Link>
-          </div>
-        ))}
-      </div>
-    );
-  } catch (error: any) {
-    return <div>Failed to load: {(error as Error).message}</div>; 
-  }
+  const data = await getBooks();
+
+  return (
+    <div>
+      <h1>Books</h1>
+      {data.map((book) => (
+        <div key={book.id}>
+          <Link href={`/books/${book.id}`}>
+            <a>{book.title}</a>
+          </Link>
+        </div>
+      ))}
+    </div>
+  );
 }
